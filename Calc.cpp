b77e6be527292151f9cf2106d16f67b61dff67fd
prethiv <prethiv191@gmail.com>
2022-02-27 20:04:50 +0530

    Calculator Stubbing

---
+++ Calc.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int main(){
+
+
+    int a,b;
+    cin>>a>>b;
+
+    int choice;
+    cin>>choice;
+
+    if(choice==1){
+        add(a,b);
+    }
+    else if(choice==2){
+        sub(a,b);
+    }
+    else if(choice==3){
+        div(a,b);
+    }
+    else{
+        mul(a,b);
+    }
+
+}
